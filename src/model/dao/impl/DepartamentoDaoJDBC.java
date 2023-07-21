7003042761b6f82a4421b8f1aa3ef57c2f87bf39
Leonardo Macedo <leo.depaula2019@gmail.com>
2023-07-21 18:51:14 -0300

    DepartamentoDaoJDBC implementado, mas erro no atualizarDepartamento

--- src/model/dao/impl/DepartamentoDaoJDBC.java
+++ src/model/dao/impl/DepartamentoDaoJDBC.java
@@ -6,6 +6,7 @@ import model.dao.DepartamentoDao;
 import model.entities.Departamento;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DepartamentoDaoJDBC implements DepartamentoDao {
@@ -17,7 +18,8 @@ public class DepartamentoDaoJDBC implements DepartamentoDao {
     public void inserirDepartamento(Departamento departamento) {
         try (Connection connection = DB.getConnection()) {
             String sql = "INSERT INTO departamento (nome) VALUES (?)";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            try (PreparedStatement preparedStatement =
+                         connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
                 preparedStatement.setString(1, departamento.getNome());
 
@@ -38,23 +40,82 @@ public class DepartamentoDaoJDBC implements DepartamentoDao {
         }
     }
 
+    //Atualizar departamento pelo ID
     @Override
     public void atualizarDepartamento(Departamento departamento) {
+        try (Connection connection = DB.getConnection()) {
+            String sql = "UPDATE departamento SET nome = ? WHERE id = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setString(1, departamento.getNome());
+                preparedStatement.setInt(2, departamento.getId());
 
+                preparedStatement.executeUpdate();
+            }
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 
+    //Deletar departamento pelo ID
     @Override
     public void removerDepartamento(Departamento departamento) {
+        try (Connection connection = DB.getConnection()) {
+            String sql = "DELETE FROM departamento WHERE id = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setInt(1, departamento.getId());
 
+                preparedStatement.executeUpdate();
+            }
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
     public Departamento encontrarDepartamentoPorId(Integer id) {
-        return null;
+        try (Connection connection = DB.getConnection()) {
+            String sql = "SELECT * FROM departamento WHERE id = ?";
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setInt(1, id);
+
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    Departamento departamento = new Departamento();
+                    if (resultSet.next()) {
+                        departamento.setId(resultSet.getInt("id"));
+                        departamento.setNome(resultSet.getString("nome"));
+                        return departamento;
+
+                    }
+                    return null;
+                }
+            }
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
     public List<Departamento> todosDepartamentos() {
-        return null;
+        try (Connection connection = DB.getConnection()) {
+            String sql = "SELECT * FROM departamento";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                 ResultSet resultSet = preparedStatement.executeQuery()) {
+                    List<Departamento> listaDepartamentos = new ArrayList<>();
+                    while (resultSet.next()) {
+                        Departamento departamento = new Departamento();
+                        departamento.setId(resultSet.getInt("id"));
+                        departamento.setNome(resultSet.getString("nome"));
+                        listaDepartamentos.add(departamento);
+                    }
+                    return listaDepartamentos;
+            }
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 }
